6cccd5b2657554ed5ab2267f92a44e80281b27bd
Juan Carlos Valdes <juancarlos.valdez@externos-cl.cencosud.com>
2021-09-14 16:26:45 -0300

    feat: proyecto base sin redux para comenzar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "font-awesome/css/font-awesome.css";
+import reportWebVitals from "./reportWebVitals";
+let WithRouter = () => (
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
+
+ReactDOM.render(<WithRouter />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
